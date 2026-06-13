657c364b2cbe216509d7c4ca7740b76020121789
Davide Clemente <1190497@isep.ipp.pt>
2026-06-13 13:55:03 +0100

    Add Manage Sources section to Settings for hiding/unhiding audio sessions

--- Core/Views/SettingsPage.xaml.cs
+++ Core/Views/SettingsPage.xaml.cs
@@ -1,5 +1,7 @@
 using System.IO.Ports;
+using AudioMixerWin.Core.Models;
 using AudioMixerWin.Core.ViewModels;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace AudioMixerWin.Core.Views;
@@ -16,5 +18,26 @@ public sealed partial class SettingsPage : Page
     {
         ViewModel = viewModel;
         InitializeComponent();
+    }
+
+    private void OnHideClick(object sender, RoutedEventArgs e)
+    {
+        if (((Button)sender).Tag is not AudioSession session)
+            return;
+
+        var blocked = ViewModel.HideSession(session);
+        if (blocked is not null)
+        {
+            HideInfoBar.Message = blocked;
+            HideInfoBar.IsOpen = true;
+        }
+    }
+
+    private void OnShowClick(object sender, RoutedEventArgs e)
+    {
+        if (((Button)sender).Tag is not string processName)
+            return;
+
+        ViewModel.UnhideProcessCommand.Execute(processName);
     }
 }
